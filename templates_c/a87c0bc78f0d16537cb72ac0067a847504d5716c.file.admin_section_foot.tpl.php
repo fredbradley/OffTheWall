d2ec5396ec48f33e3dc581feaf3cc92d700a37b3
Fred Bradley <hello@fredbradley.co.uk>
2013-01-07 12:21:48 +0000

    Local Sync

---
+++ templates_c/a87c0bc78f0d16537cb72ac0067a847504d5716c.file.admin_section_foot.tpl.php
@@ -0,0 +1,30 @@
+<?php /* Smarty version Smarty-3.1.11, created on 2013-01-07 12:19:13
+         compiled from "/Users/fredbradley/Sites/offthewall.com.au/doc_root/templates/admin_section_foot.tpl" */ ?>
+<?php /*%%SmartyHeaderCode:172750100950eabd41a7a887-17094043%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    'a87c0bc78f0d16537cb72ac0067a847504d5716c' => 
+    array (
+      0 => '/Users/fredbradley/Sites/offthewall.com.au/doc_root/templates/admin_section_foot.tpl',
+      1 => 1357554595,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '172750100950eabd41a7a887-17094043',
+  'function' => 
+  array (
+  ),
+  'has_nocache_code' => false,
+  'version' => 'Smarty-3.1.11',
+  'unifunc' => 'content_50eabd41a7b971_22994898',
+),false); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_50eabd41a7b971_22994898')) {function content_50eabd41a7b971_22994898($_smarty_tpl) {?>	<footer>
+		<div class="wrapper">
+			<span class="copyright">
+			<!-- Template by:	COPYRIGHT © 2012 Mahieddine Abd-kader -->
+			</span>
+		</div>
+	</footer>
+</body>
+</html><?php }} ?>
