d7447a9a52a0f9bc4185278caeddee944d565c21
Fred Bradley <hello@fredbradley.co.uk>
2013-02-15 09:01:49 +0000

    Update to Main Site

--- templates_c/a336dc7e511e75038af62f3f28d5240648284f9b.file.home.tpl.php
+++ templates_c/a336dc7e511e75038af62f3f28d5240648284f9b.file.home.tpl.php
@@ -1,30 +1,30 @@
-<?php /* Smarty version Smarty-3.1.11, created on 2013-01-21 10:39:23
+<?php /* Smarty version Smarty-3.1.11, created on 2013-01-25 15:51:45
          compiled from "/Users/fredbradley/Sites/offthewall.com.au/doc_root/templates/home.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:179871320450fd1adb033ba8-67863978%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:17944385045102aa111289a9-04259900%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     'a336dc7e511e75038af62f3f28d5240648284f9b' => 
     array (
       0 => '/Users/fredbradley/Sites/offthewall.com.au/doc_root/templates/home.tpl',
-      1 => 1358506486,
+      1 => 1359129007,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '179871320450fd1adb033ba8-67863978',
+  'nocache_hash' => '17944385045102aa111289a9-04259900',
   'function' => 
   array (
   ),
   'has_nocache_code' => false,
   'version' => 'Smarty-3.1.11',
-  'unifunc' => 'content_50fd1adb0633c5_47890994',
+  'unifunc' => 'content_5102aa11161ac8_66190539',
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_50fd1adb0633c5_47890994')) {function content_50fd1adb0633c5_47890994($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ("admin_HTML_head.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array('title'=>"Off The Wall Example"), 0);?>
+<?php if ($_valid && !is_callable('content_5102aa11161ac8_66190539')) {function content_5102aa11161ac8_66190539($_smarty_tpl) {?><?php echo $_smarty_tpl->getSubTemplate ("admin_HTML_head.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array('title'=>"Coming Soon"), 0);?>
 
 <body>
 	<div class="wrapper contents_wrapper">
 		<div class="g_12 otwlogo">
-			<img src="/assets/images/OffTheWall/OffTheWall.png" />
+			<img src="/assets/images/OffTheWall/OTWGroupLogoLarge.png" />
 		</div>
 		<div class="g_12">
 			<span class="label">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec lacinia, felis eget scelerisque fermentum, arcu libero mollis tortor, vitae blandit velit nulla eget nisl. Morbi non massa diam. Donec dictum consectetur mi id luctus. Nam in condimentum orci. Curabitur et elit vel est tincidunt auctor. Aenean dignissim tempus eros a feugiat. Duis vulputate adipiscing consequat.</span>
@@ -36,16 +36,16 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 				<h2>Current</h2>
 			</div>
 			<div class="g_3 otwlogo">
-				<img src="/assets/images/OffTheWall/OffTheWall.png" />
+				<img src="/assets/images/OffTheWall/OTWGroupLogo.jpg" />
 			</div>
 			<div class="g_3 otwlogo">
-				<img src="/assets/images/OffTheWall/OffTheWall.png" />
+				<img src="/assets/images/OffTheWall/OTWGroupLogo.jpg" />
 			</div>
 			<div class="g_3 otwlogo">
-				<img src="/assets/images/OffTheWall/OffTheWall.png" />
+				<img src="/assets/images/OffTheWall/OTWGroupLogo.jpg" />
 			</div>
 			<div class="g_3 otwlogo">
-				<img src="/assets/images/OffTheWall/OffTheWall.png" />
+				<img src="/assets/images/OffTheWall/OTWGroupLogo.jpg" />
 			</div>
 		</div>
 		<div class="g_4">
@@ -53,18 +53,16 @@ $_valid = $_smarty_tpl->decodeProperties(array (
 				<h2>Old</h2>
 			</div>
 			<div class="g_6 otwlogo">
-				<img src="/assets/images/OffTheWall/OffTheWall.png" />
+				<img src="/assets/images/OffTheWall/OTWGroupLogo.jpg" />
 			</div>
 			<div class="g_6 otwlogo">
-				<img src="/assets/images/OffTheWall/OffTheWall.png" />
+				<img src="/assets/images/OffTheWall/OTWGroupLogo.jpg" />
 			</div>
 		</div>		
 		<div class="g_12">
 			<p class="label c">hello@offthewall.com.au</p>
 		</div>
-	</div>
-	
-	
+	</div>	
 <?php echo $_smarty_tpl->getSubTemplate ("admin_section_foot.tpl", $_smarty_tpl->cache_id, $_smarty_tpl->compile_id, null, null, array(), 0);?>
 
 <?php }} ?>
--- templates_c/a87c0bc78f0d16537cb72ac0067a847504d5716c.file.admin_section_foot.tpl.php
+++ templates_c/a87c0bc78f0d16537cb72ac0067a847504d5716c.file.admin_section_foot.tpl.php
@@ -1,43 +1,43 @@
-<?php /* Smarty version Smarty-3.1.11, created on 2013-01-21 10:39:23
+<?php /* Smarty version Smarty-3.1.11, created on 2013-02-15 08:59:12
          compiled from "/Users/fredbradley/Sites/offthewall.com.au/doc_root/templates/admin_section_foot.tpl" */ ?>
-<?php /*%%SmartyHeaderCode:129619559550fd1adb06f9e2-16575987%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+<?php /*%%SmartyHeaderCode:1129837114511df8e0ba21e3-40989958%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
 $_valid = $_smarty_tpl->decodeProperties(array (
   'file_dependency' => 
   array (
     'a87c0bc78f0d16537cb72ac0067a847504d5716c' => 
     array (
       0 => '/Users/fredbradley/Sites/offthewall.com.au/doc_root/templates/admin_section_foot.tpl',
-      1 => 1358340936,
+      1 => 1359133144,
       2 => 'file',
     ),
   ),
-  'nocache_hash' => '129619559550fd1adb06f9e2-16575987',
+  'nocache_hash' => '1129837114511df8e0ba21e3-40989958',
   'function' => 
   array (
   ),
   'has_nocache_code' => false,
   'version' => 'Smarty-3.1.11',
-  'unifunc' => 'content_50fd1adb0729b9_79684360',
+  'unifunc' => 'content_511df8e0bacf13_11666526',
 ),false); /*/%%SmartyHeaderCode%%*/?>
-<?php if ($_valid && !is_callable('content_50fd1adb0729b9_79684360')) {function content_50fd1adb0729b9_79684360($_smarty_tpl) {?>	<footer>
-		<div class="wrapper">
-			<span class="copyright">
-			<!-- Template by:	COPYRIGHT © 2012 Mahieddine Abd-kader -->
-			</span>
-		</div>
-	</footer>
+<?php if ($_valid && !is_callable('content_511df8e0bacf13_11666526')) {function content_511df8e0bacf13_11666526($_smarty_tpl) {?><?php  $_config = new Smarty_Internal_Config("test.conf", $_smarty_tpl->smarty, $_smarty_tpl);$_config->loadConfigVars(null, 'local'); ?>
+
 	<div class="bottom">
-		<div class="wrapper">
-			
-<div class="g_10">
-			<span class="copyright">
-			 A product developed by <a class="copyright" href="http://www.fredbradley.me/?utm_source=hosting&utm_medium=sitebuilder&utm_term=offthewall&utm_content=offthewall&utm_campaign=sitebuilder">Fred Bradley</a>. </span>
-		</div>
+		<div class="wrapper fredsbit">
+			<div class="g_10">
+				<span class="label offthewall">&copy; <?php echo $_smarty_tpl->getConfigVariable('title');?>
+<br />Website developed by <a class="label offthewall" 
+href="<?php echo $_smarty_tpl->getConfigVariable('fredbradleylink');?>
+">Fred Bradley</a>.</span>
+			</div>
 			<div class="g_2" title="Visit Fred's Site">
-					<a href="http://www.fredbradley.me/?utm_source=hosting&utm_medium=sitebuilder&utm_term=offthewall&utm_content=offthewall&utm_campaign=sitebuilder" title="Visit Fred's Site">
+					<a href="<?php echo $_smarty_tpl->getConfigVariable('fredbradleylink');?>
+" title="Visit Fred's Site">
 						<img src="http://www.fredbradley.co.uk/wp-content/uploads/2012/05/Justname-dark-bw-e1335890740674.png" alt="Fred Bradley Logo" title="Visit Fred's Site" style="width:100%;margin-bottom:10px;" />
 					</a>
-				</div>		</div>
+			</div>
+		</div>
 	</div>
 </body>
-</html><?php }} ?>
+</html>
+			<!-- Template by:	COPYRIGHT © 2012 Mahieddine Abd-kader -->
+<?php }} ?>
